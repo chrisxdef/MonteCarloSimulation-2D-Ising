9d12cfe08b7da530637b7a55bf8dda745b55bc9c
Christopher Heisler <cheisler72@gmail.com>
2017-04-16 12:03:36 -0400

    Plot titles

--- python/plotResults.py
+++ python/plotResults.py
@@ -4,10 +4,10 @@ import sys
 
 df = pd.read_csv(sys.argv[1])
 
-df.plot(kind='scatter', x='temperature', y='ave_magnetization')
-df.plot(kind='scatter', x='temperature', y='ave_magnetization^2')
-df.plot(kind='scatter', x='temperature', y='susceptibility')
-df.plot(kind='scatter', x='temperature', y='ave_energy')
-df.plot(kind='scatter', x='temperature', y='ave_energy^2')
-df.plot(kind='scatter', x='temperature', y='C_v')
+df.plot(kind='scatter', x='temperature', y='ave_magnetization', title='Average Magnetization')
+df.plot(kind='scatter', x='temperature', y='ave_magnetization^2', title='Average Magnetization^2')
+df.plot(kind='scatter', x='temperature', y='susceptibility', title='Susceptibility')
+df.plot(kind='scatter', x='temperature', y='ave_energy', title='Average Energy')
+df.plot(kind='scatter', x='temperature', y='ave_energy^2', title='Average Energy^2')
+df.plot(kind='scatter', x='temperature', y='C_v', title='Specific Heat')
 plt.show()
